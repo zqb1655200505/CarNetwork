ac9d5684d86693a4525a371a9a95a66711c89fdb
zqb1655200505 <1655200505@qq.com>
2016-07-10 16:33:19 +0800

    增加了JPush

--- app/src/main/java/com/zqb/carnetwork/MyApplication.java
+++ app/src/main/java/com/zqb/carnetwork/MyApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.baidu.mapapi.SDKInitializer;
 
+import cn.jpush.android.api.JPushInterface;
+
 /**
  * Created by zqb on 2016/6/4.
  */
@@ -12,6 +14,8 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        JPushInterface.setDebugMode(true);
+        JPushInterface.init(this);
         // 在使用 SDK 各组间之前初始化 context 信息，传入 ApplicationContext
         SDKInitializer.initialize(this);
     }
